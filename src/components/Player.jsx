0e1b4f47489df6c4cb18eb2d341c6d6610f7bcbc
Charlotte Menard <charlotte-menard_student2021@wilder.school>
2022-01-23 20:57:07 +0100

    Weird sounds working

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -9,7 +9,7 @@ import countReducer from "../reducers/countReducer";
 import beep1 from '../assets/beep-02.mp3';
 import beep2 from '../assets/drop-1.mp3';
 import beep3 from '../assets/button-3.mp3';
-import beep4 from '../assets/honk-1.mp3';
+import beep4 from '../assets/honk-2.mp3';
 import badBeep from '../assets/beep-03.mp3';
 import styles from './Player.module.css';
 
@@ -78,30 +78,31 @@ const Player = () => {
     }
     return new Promise ((resolve) =>{
       setTimeout(() => {
-      }, 250);
-      setTimeout(() => {
-        button.className += ` ${styles['active']}`;
         gameBeep.play();
-      }, 250);
-      setTimeout(() => {
-        button.className = button.className.replace(` ${styles['active']}`, '');
-        resolve();
-      }, 1000);
+        button.className += ` ${styles['active']}`;
+        setTimeout(() => {
+          button.className = button.className.replace(` ${styles['active']}`, '');
+          resolve();
+        }, 1000);
+      }, 150);
     });
   }
   
-  useEffect(() => {
-    const flash = async () => {
-      for (let i=0; i<gameState.length; i++) {
-          await lightOn(gameState[i]);
-        }
-      if (countState !== 0) {
-        setMainMessage("YOUR TURN");
-      }
-    };
-      flash();
-  }, [gameState])
+  const flash = async () => {
+    for (let i=0; i<gameState.length; i++) {
+      await lightOn(gameState[i]);
+    }
+    if (countState !== 0) {
+      setMainMessage("YOUR TURN");
+    }
+  };
 
+  useEffect(() => {
+    setTimeout(() => {
+      flash();
+    }, 750)
+  }, [gameState])
+  
   useEffect(() => {
     if (turnState === false) {
       gameDispatch({type:'NEXT_LEVEL'});
@@ -117,19 +118,21 @@ const Player = () => {
             stepDispatch({type:'INCREASE_STEP'});
             setMessage(`Yay ! ${gameState.length - stepState - 1} left`);
           } else {
-            if (bestRef.current < countState) {
-              bestRef.current = countState;
-              bestShot.className += ` ${styles['better']}`;
-            }
             gameDispatch({type:'RESET_GAME'});
             stepDispatch({type:'RESET_STEP', payload: stepInitialState});
             playerDispatch({type:'RESET_PLAYER'});
             turnDispatch({type:'END_GAME', payload: turnInitialState});
             countDispatch({type:'RESET_COUNT', payload: countInitialState});
-            setMainMessage('GAME RESET');
-            setMessage('Wrong answer');
-            badAudio.play();
-            messageBox.className += ` ${styles['wrong']}`;
+            setTimeout(() => {
+              if (bestRef.current < countState) {
+                bestRef.current = countState;
+                bestShot.className += ` ${styles['better']}`;
+              }
+              setMainMessage('GAME RESET');
+              setMessage('Wrong answer');
+              badAudio.play();
+              messageBox.className += ` ${styles['wrong']}`;
+            }, 300)
           }
         } else if (playerState.length === gameState.length) {
           if (playerState[stepState] === gameState[stepState]) {
@@ -138,17 +141,19 @@ const Player = () => {
             setMessage("Good game !");
             turnDispatch({type:'SIMONES_TURN'});
           } else {
-            if (bestRef.current < countState) {
-              bestRef.current = countState;
-              bestShot.className += ` ${styles['better']}`;
-            }
             gameDispatch({type:'RESET_GAME'});
             turnDispatch({type:'END_GAME', payload: turnInitialState});
             countDispatch({type:'RESET_COUNT', payload: countInitialState});
-            setMainMessage('GAME RESET');
-            setMessage(`Too bad, you were close !`);
-            badAudio.play();
-            messageBox.className += ` ${styles['wrong']}`;
+            setTimeout(() => {
+              if (bestRef.current < countState) {
+                bestRef.current = countState;
+                bestShot.className += ` ${styles['better']}`;
+              }
+              setMainMessage('GAME RESET');
+              setMessage(`Too bad, you were close !`);
+              badAudio.play();
+              messageBox.className += ` ${styles['wrong']}`;
+            }, 750)
           }
           playerDispatch({type:'RESET_PLAYER'});
           stepDispatch({type:'RESET_STEP', payload: stepInitialState});
